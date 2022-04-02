d7847138725679b84d38f26d5549cfb5d5da115a
Gabriel <gbialystokjoly@gmail.com>
2022-04-02 22:52:45 +0200

    Bug Fixes

--- StoryLine/newstory.py
+++ StoryLine/newstory.py
@@ -22,9 +22,8 @@ def newMain():
     clear()
     if finput2() == False:
         return False
-    clear()
     input3 = input("That evening, after your nap, you decide to go out for a nice dinner to treat yourself as at work the previous day you got a raise. At the restaurant, you order and everything is very good, although you feel sick after. You assume you just ate something that was a bit off, but it rapidly turns into a stomach churning feeling and you feel like you’re about to vomit your whole body out. You can Call poison control, Vomit in a restaurant tiolet, Scream for help or Hope it's nothing and return home").lower()
-    if input3 == "call poison control":
+    if input3 == "call poison control ":
         rnum = random.randint(1, 2)
         if rnum == 1:
             print("Poison control shows up and they cure you")#next
@@ -39,13 +38,14 @@ def newMain():
         print("No one comes to your aid, you die from food poisoning.")
         return False
     else:
-        print("On your way home, you drop unconscious to the floor, you wake up in a hospital ")
+        print("On your way home, you drop unconscious to the floor.")
         print("You wake up in a hospital, the doctors tell you that you almost died of food poisoning, but you can go home now. You go home, but realize that it’s already 10:45 AM. You’re late to work.")
+        input("\nPress enter to continue")
     clear()
     input4 = input("You realize that you have to go to work no matter what, now you finally have an easy choice. \n You can Take the bus a94 to your work. Take the train 6WA to your work or Walk to your job").lower()
-    if input4 == "take the bus a94 to your work":
+    if input4 == "take the bus A94 to your work":
         print("Once you get on the bus, the driver says that it will be making a detour back to the main bus station because of some minor issues. You, realizing that this would making you late, decide to hop on the train 6WA. You arrive an hour late to work. ")#next
-    elif input4 == "take the train 6wa to your work":
+    elif input4 == "take the train 6WA to your work":
         rnum = random.randint(1, 3)
         if rnum == 1:
             print("The train makes it to your office, but you’re 50 minutes late.")#next
@@ -76,3 +76,4 @@ def finput2():
         else:
             print("You enter your basement but trip on the stairs and break your spine.")
             return False
+    clear()
